25ca3124d5330a033c6ded3ee28d54eccaa71f4d
Mohammad Alshetwi <59418749+malshetwi@users.noreply.github.com>
2021-09-16 01:09:30 +0300

    Update UltrasonicSensor.ino

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,27 +1,16 @@
 #include <NewPing.h>                                                                 //UltraSonic library.
 
-
-
 int trig = D6 ;                                                                     // Variable for U_Sonic, depend in the pin you connect.
 int echo = D5 ;                                                                     // Variable for U_Sonic, depend in the pin you connect.
 int max_dis = 200 ;                                                                 // Variable for U_Sonic.
 NewPing sonic (trig , echo , max_dis);                                              // Variables in library(parameters).
 
-
-
-
 void setup() { // code here, to run once: 
                    
 Serial.begin(9600);                                                                 // Debug console
-
-  
-
 }
 
-
 void loop() {  // code here, to run repeatedly:
-
-
 
   int us = sonic.ping();                                                            // Variable in library to mathematical calculate.
   int dis = us / US_ROUNDTRIP_CM ;                                                  // compelete equation of calculate bring (CM).
@@ -29,8 +18,4 @@ void loop() {  // code here, to run repeatedly:
   Serial.print("Water Level:");                                                     // printing Text.
   Serial.println(dis);                                                              // printing int. Variable.
   delay(200);                                                                       // delay time.
-  
-
-
-
 }
